6abce78b62e5f42008f17617b7b1df335ebb06a3
Sherzod Musurmonov <102805587+sherzot@users.noreply.github.com>
2024-09-03 08:53:04 +0900

    update

--- js/main.js
+++ js/main.js
@@ -102,7 +102,7 @@
         dots: false,
         nav: true,
         navText: ["<span class='fa fa-angle-left'><span/>", "<span class='fa fa-angle-right'><span/>"],
-        smartSpeed: 1200,
+        smartSpeed: 1000,
         autoHeight: false,
         autoplay: true
     });
